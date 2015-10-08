38661029c8f0f872c45c7ce30b5b9643c54442c1
MatSchaeff <the.arthek@gmail.com>
2015-10-09 01:15:37 +0200

    try with local Json file

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,13 @@
 QUnit.test('assertions', function(assert) {
     assert.ok(1 == 1, 'one equals one');
 });
-
  QUnit.test("should get a nextprot valid id", function (assert) {
     var done = assert.async();
      console.log("begin async");
-    $.getJSON("https://api.nextprot.org/entries/search/peptide?peptide=FVNQHLCGSH&modeIL=true&clientInfo=nextprotTeam&applicationName=PeptideViewer.json", function( data ) {
+    $.getJSON("overview.json", function( data ) {
         console.log("entering the json");
         console.log(data);
-        assert.equal("NX_F8WCM5", data[0].uniqueName, 'expect 18 properties');
+        assert.equal("NX_P01308", data.entry.uniqueName, 'expect 18 properties');
         done();
     }, function (error) {
         console.log("error");
