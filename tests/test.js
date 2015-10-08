3862d3f15fdd1ae0b96a6f2488d74299a507c924
Mathieu Schaeffer <the.arthek@gmail.com>
2015-10-08 17:08:22 +0200

    test smthg else

--- tests/test.js
+++ tests/test.js
@@ -4,11 +4,15 @@ QUnit.test('assertions', function(assert) {
 
  QUnit.test("should get a nextprot valid id", function (assert) {
     var done = assert.async();
+     console.log("begin async");
     $.getJSON("https://api.nextprot.org/entry/NX_P01308/overview.json", function( data ) {
+        console.log("entering the json");
         console.log(data);
         assert.equal("NX_P01308", data.entry.uniqueName, 'expect 18 properties');
         done();
     }, function (error) {
+        console.log("error");
+        console.log(error);
         assert.notOk("failed to load...");
         done();
     });
